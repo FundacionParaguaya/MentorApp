0156149cb53fc751dde573bd6489d786da8c597c
Iva Kop <iva@penguin.digital>
2018-12-19 16:10:44 +0200

    Remove console error

--- src/screens/lifemap/Question.js
+++ src/screens/lifemap/Question.js
@@ -72,7 +72,6 @@ export class Question extends Component {
         survey: this.survey
       })
     } else if (this.step + 1 >= this.indicators.length && answer === 0) {
-      console.log(answer)
       return this.props.navigation.navigate('Skipped', {
         draftId: this.draftId,
         survey: this.survey
