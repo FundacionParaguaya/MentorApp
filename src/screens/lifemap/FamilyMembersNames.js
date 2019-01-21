87e521df0cbc216c2fdb2f5d62ba48d5b78a359e
Iva Kop <iva@penguin.digital>
2019-01-21 18:40:14 +0200

    Fix translation

--- src/screens/lifemap/FamilyMembersNames.js
+++ src/screens/lifemap/FamilyMembersNames.js
@@ -24,38 +24,6 @@ export class FamilyMembersNames extends Component {
 
   state = { errorsDetected: [], showErrors: false }
 
-  familyMembersCountArray = [
-    { text: this.props.t('views.family.onlyPerson'), value: 1 },
-    { text: '2', value: 2 },
-    { text: '3', value: 3 },
-    { text: '4', value: 4 },
-    { text: '5', value: 5 },
-    { text: '6', value: 6 },
-    { text: '7', value: 7 },
-    { text: '8', value: 8 },
-    { text: '9', value: 9 },
-    { text: '10', value: 10 },
-    { text: '11', value: 11 },
-    { text: '12', value: 12 },
-    { text: '13', value: 13 },
-    { text: '14', value: 14 },
-    { text: '15', value: 15 },
-    { text: '16', value: 16 },
-    { text: '17', value: 17 },
-    { text: '18', value: 18 },
-    { text: '19', value: 19 },
-    { text: '20', value: 20 },
-    { text: '21', value: 21 },
-    { text: '22', value: 22 },
-    { text: '23', value: 23 },
-    { text: '24', value: 24 },
-    { text: '25', value: 25 },
-    {
-      text: this.props.t('views.family.preferNotToSay'),
-      value: -1
-    }
-  ]
-
   componentDidMount() {
     this.props.addDraftProgress(this.draftId, {
       screen: 'FamilyMembersNames'
@@ -167,10 +135,44 @@ export class FamilyMembersNames extends Component {
     })
   }
 
+  getFamilyMembersCountArray = t => [
+    { text: t('views.family.onlyPerson'), value: 1 },
+    { text: '2', value: 2 },
+    { text: '3', value: 3 },
+    { text: '4', value: 4 },
+    { text: '5', value: 5 },
+    { text: '6', value: 6 },
+    { text: '7', value: 7 },
+    { text: '8', value: 8 },
+    { text: '9', value: 9 },
+    { text: '10', value: 10 },
+    { text: '11', value: 11 },
+    { text: '12', value: 12 },
+    { text: '13', value: 13 },
+    { text: '14', value: 14 },
+    { text: '15', value: 15 },
+    { text: '16', value: 16 },
+    { text: '17', value: 17 },
+    { text: '18', value: 18 },
+    { text: '19', value: 19 },
+    { text: '20', value: 20 },
+    { text: '21', value: 21 },
+    { text: '22', value: 22 },
+    { text: '23', value: 23 },
+    { text: '24', value: 24 },
+    { text: '25', value: 25 },
+    {
+      text: t('views.family.preferNotToSay'),
+      value: -1
+    }
+  ]
+
   render() {
     const { t } = this.props
     const { showErrors } = this.state
     const draft = this.getDraft()
+
+    const familyMembersCountArray = this.getFamilyMembersCountArray(t)
 
     const familyMembersCount =
       draft.familyData.countFamilyMembers &&
@@ -196,7 +198,7 @@ export class FamilyMembersNames extends Component {
             value={this.getFieldValue('countFamilyMembers') || ''}
             detectError={this.detectError}
             showErrors={showErrors}
-            options={this.familyMembersCountArray}
+            options={familyMembersCountArray}
           />
           <TextInput
             validation="string"
