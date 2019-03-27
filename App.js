bb6c27d8abaeac781287559460ef787c972e89e4
Dan Atanasov <danmanm@gmail.com>
2019-03-27 20:14:32 +0200

    Revert package versions

--- App.js
+++ App.js
@@ -5,14 +5,9 @@ import SplashScreen from 'react-native-splash-screen'
 /* eslint-disable import/no-unresolved */
 import BackgroundTask from 'react-native-background-task'
 /* eslint-enable import/no-unresolved */
-import MapboxGL from '@mapbox/react-native-mapbox-gl'
 import NavigationWrapper from './src/components/NavWrapper'
 import store from './src/redux/store'
 import { initImageCaching } from './src/cache'
-
-MapboxGL.setAccessToken(
-  'pk.eyJ1IjoiZGFubWFuNyIsImEiOiJjanQ4bTU0cjIwMmdkNDRtbnppdWoyNm81In0.SO7wd6FjGH2qtwXp7MGNRg'
-)
 
 // schedule a background task to check the images cache
 BackgroundTask.define(() => {
