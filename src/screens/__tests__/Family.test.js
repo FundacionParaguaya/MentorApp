20b92e23d2fa62390f3bc05e70ce1005da48f81d
Dan Atanasov <danmanm@gmail.com>
2019-05-10 16:53:18 +0300

    Family tests

--- src/screens/__tests__/Family.test.js
+++ src/screens/__tests__/Family.test.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import { FlatList } from 'react-native'
 import { shallow } from 'enzyme'
 import { Family } from '../Family'
 import FamilyTab from '../../components/FamilyTab'
 import data from '../__mocks__/fake-socio-economic-data.json'
 import RoundImage from '../../components/RoundImage'
 import Button from '../../components/Button'
+import FamilyListItem from '../../components/FamilyListItem'
+
 const createTestProps = props => ({
   t: value => value,
   updateNav: jest.fn(),
   navigation: {
     setParams: jest.fn(),
     replace: jest.fn(),
+    navigate: jest.fn(),
     getParam: param => {
       switch (param) {
         case 'survey':
@@ -18,6 +22,12 @@ const createTestProps = props => ({
 
         case 'activeTab':
           return 'Details'
+
+        case 'isDraft':
+          return true
+
+        case 'familyName':
+          return 'Test Family'
 
         default:
           return {
@@ -102,6 +112,13 @@ describe('Single Family View', () => {
         .props()
         .onPress()
       expect(wrapper.instance().state.activeTab).toBe('LifeMap')
+
+      wrapper
+        .find(FamilyTab)
+        .at(0)
+        .props()
+        .onPress()
+      expect(wrapper.instance().state.activeTab).toBe('Details')
     })
     it('changing the active tab changes the view', () => {
       wrapper.setState({ activeTab: 'LifeMap' })
@@ -129,6 +146,110 @@ describe('Single Family View', () => {
           socioEconomics: undefined,
           step: undefined,
           survey: { id: 1 }
+        }
+      )
+    })
+
+    it('navigates to family location', () => {
+      wrapper
+        .find(FamilyListItem)
+        .first()
+        .props()
+        .handleClick()
+
+      expect(props.navigation.navigate).toHaveBeenCalledWith('Location', {
+        family: expect.any(Object)
+      })
+    })
+
+    it('renders a list of family members and handles navigation to those members pages', () => {
+      const data = wrapper.find(FlatList).props().data
+
+      expect(
+        wrapper
+          .find(FlatList)
+          .props()
+          .keyExtractor(data[0], 0)
+      ).toEqual('0')
+
+      wrapper
+        .find(FlatList)
+        .props()
+        .renderItem({ item: { name: 'Juan' }, index: 0 })
+        .props.handleClick()
+
+      expect(props.navigation.navigate).toHaveBeenCalledWith(
+        'FamilyParticipant',
+        {
+          family: expect.any(Object)
+        }
+      )
+
+      wrapper
+        .find(FlatList)
+        .props()
+        .renderItem({ item: { name: 'Ana' }, index: 1 })
+        .props.handleClick()
+
+      expect(props.navigation.navigate).toHaveBeenCalledWith('FamilyMember', {
+        member: expect.any(Object)
+      })
+    })
+
+    it('navigates to socio economic category review', () => {
+      props = createTestProps({
+        navigation: {
+          setParams: jest.fn(),
+          replace: jest.fn(),
+          navigate: jest.fn(),
+          getParam: param => {
+            switch (param) {
+              case 'survey':
+                return data
+
+              case 'activeTab':
+                return 'Details'
+
+              case 'familyName':
+                return 'Test Family'
+
+              case 'isDraft':
+                return false
+
+              default:
+                return {
+                  draftId: 1,
+                  surveyId: 1,
+                  progress: { screen: 'FamilyMembersNames' },
+                  createdAt: 1,
+                  status: 'Draft',
+                  familyData: {
+                    familyMembersList: [
+                      {
+                        firstName: 'Juan',
+                        lastName: 'Perez'
+                      }
+                    ]
+                  }
+                }
+            }
+          }
+        }
+      })
+
+      wrapper = shallow(<Family {...props} />)
+
+      wrapper
+        .find(FamilyListItem)
+        .last()
+        .props()
+        .handleClick()
+
+      expect(props.navigation.navigate).toHaveBeenCalledWith(
+        'SocioEconomicQuestion',
+        {
+          family: expect.any(Object),
+          page: 2
         }
       )
     })
