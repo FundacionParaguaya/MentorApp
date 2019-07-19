6ba367c0dbe1ec8d2a403e7dfa1c49acac99c77a
Vasil Hristov <vasildimitrovhristov@gmail.com>
2019-07-19 08:02:05 +0300

    refactored setScreen method

--- src/screens/lifemap/SocioEconomicQuestion.js
+++ src/screens/lifemap/SocioEconomicQuestion.js
@@ -161,12 +161,11 @@ export class SocioEconomicQuestion extends Component {
           draft: this.state.draft
         })
       : this.props.navigation.push('SocioEconomicQuestion', {
-          socioEconomics: this.setScreens(socioEconomics, -1),
-          // socioEconomics: {
-          //   currentScreen: socioEconomics.currentScreen - 1,
-          //   questionsPerScreen: socioEconomics.questionsPerScreen,
-          //   totalScreens: socioEconomics.totalScreens
-          // },
+          socioEconomics: {
+            currentScreen: this.setScreen(-1),
+            questionsPerScreen: socioEconomics.questionsPerScreen,
+            totalScreens: socioEconomics.totalScreens
+          },
           survey: this.survey,
           draft: this.state.draft
         })
@@ -254,12 +253,11 @@ export class SocioEconomicQuestion extends Component {
         : this.props.navigation.push('SocioEconomicQuestion', {
             survey: this.survey,
             draft: this.state.draft,
-            socioEconomics: this.setScreens(socioEconomics, 1)
-            // socioEconomics: {
-            //   currentScreen: socioEconomics.currentScreen + 1,
-            //   questionsPerScreen: socioEconomics.questionsPerScreen,
-            //   totalScreens: socioEconomics.totalScreens
-            // }
+            socioEconomics: {
+              currentScreen: this.setScreen(1),
+              questionsPerScreen: socioEconomics.questionsPerScreen,
+              totalScreens: socioEconomics.totalScreens
+            }
           })
     }
   }
@@ -300,52 +298,45 @@ export class SocioEconomicQuestion extends Component {
     }
   }
 
-  setScreens = (screenData, step) => {
-    const QUESTIONS_FOR_SCREEN =
-      screenData.questionsPerScreen[screenData.currentScreen]
-    // const TOTAL_SCREENS = screenData.totalScreens
+  setScreen = step => {
+    const SCREEN_DATA = this.props.navigation.getParam('socioEconomics')
     const SKIP_SCREEN_WITH_ONE_STEP = step
+
+    /* 'fromBeginLifemap' when pressing baack on the last screen we receive 
+    current screen === totalScreens : 
+      to get actual data for current -1 , 
+      to get previous -1  */
+    const QUESTIONS_FOR_SCREEN =
+      SCREEN_DATA.questionsPerScreen[
+        SCREEN_DATA.currentScreen === SCREEN_DATA.totalScreens && step === -1
+          ? SCREEN_DATA.currentScreen - 2
+          : SCREEN_DATA.currentScreen
+      ]
     if (
       !(
-        QUESTIONS_FOR_SCREEN.forFamily &&
-        QUESTIONS_FOR_SCREEN.forFamily.length > 0
+        QUESTIONS_FOR_SCREEN.forFamily && QUESTIONS_FOR_SCREEN.forFamily.length
       ) &&
       QUESTIONS_FOR_SCREEN.forFamilyMember &&
-      QUESTIONS_FOR_SCREEN.forFamilyMember.length > 0
+      QUESTIONS_FOR_SCREEN.forFamilyMember.length
     ) {
       const { familyMembersList } = this.state.draft.familyData
-      let atLeastOneMemberHasQuestions = false
-      familyMembersList.forEach((_member, index) => {
-        atLeastOneMemberHasQuestions =
-          atLeastOneMemberHasQuestions ||
-          familyMemberWillHaveQuestions(
+
+      const atLeastOneMemberHasQuestions = familyMembersList.some(
+        (_member, index) => {
+          return familyMemberWillHaveQuestions(
             QUESTIONS_FOR_SCREEN,
             this.state.draft,
             index
           )
-      })
+        }
+      )
 
-      if (!atLeastOneMemberHasQuestions) {
-        return {
-          currentScreen:
-            screenData.currentScreen + step + SKIP_SCREEN_WITH_ONE_STEP,
-          questionsPerScreen: screenData.questionsPerScreen,
-          totalScreens: screenData.totalScreens
-        }
-      } else {
-        return {
-          currentScreen: screenData.currentScreen + step,
-          questionsPerScreen: screenData.questionsPerScreen,
-          totalScreens: screenData.totalScreens
-        }
-      }
+      return !atLeastOneMemberHasQuestions
+        ? SCREEN_DATA.currentScreen + step + SKIP_SCREEN_WITH_ONE_STEP
+        : SCREEN_DATA.currentScreen + step
     }
 
-    return {
-      currentScreen: screenData.currentScreen + step,
-      questionsPerScreen: screenData.questionsPerScreen,
-      totalScreens: screenData.totalScreens
-    }
+    return SCREEN_DATA.currentScreen + step
   }
 
   updateEconomicAnswer = (question, value, memberIndex) => {
