f3bf20a41c1907bd432dd5a7ab9c51348371f3f9
Vasil Hristov <vasildimitrovhristov@gmail.com>
2019-07-19 06:05:49 +0300

    Added custom enqueue action

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,11 +6,22 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { rootReducer } from './reducer'
 import { setHydrated } from './actions'
 import { setLanguage } from '../i18n'
+import defaultQueue from '@redux-offline/redux-offline/lib/defaults/queue'
+
 let rehydrated = false
 
 export const getHydrationState = () => rehydrated
 
 const setHydratedState = () => store.dispatch(setHydrated())
+
+const removeKeyFromObject = (obj, omitKey) => {
+  return Object.keys(obj).reduce((result, key) => {
+    if (key !== omitKey) {
+      result[key] = obj[key]
+    }
+    return result
+  }, {})
+}
 
 const store = createStore(
   rootReducer,
@@ -20,12 +31,34 @@ const store = createStore(
       persistOptions: {
         blacklist: ['hydration', 'nav']
       },
+
       // this fires after store hydration is done
       persistCallback: () => {
         setLanguage()
         setHydratedState()
       },
-      retry: () => 300000 // retry  every 5 minutes
+      retry: () => 300000, // retry  every 5 minutes
+      queue: {
+        ...defaultQueue,
+        enqueue(outbox, action) {
+          return action.type !== 'SUBMIT_DRAFT'
+            ? [...outbox, action]
+            : outbox.filter(
+                outboxAction =>
+                  outboxAction.id !== action.id &&
+                  outboxAction.payload.draftId !== action.payload.draftId &&
+                  JSON.stringify(
+                    removeKeyFromObject(outboxAction.meta, 'transaction')
+                  ) !==
+                    JSON.stringify(
+                      removeKeyFromObject(action.meta, 'transaction')
+                    )
+              )
+        },
+        peek(outbox) {
+          return outbox[0]
+        }
+      }
     }),
     applyMiddleware(thunk)
   )
