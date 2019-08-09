4e582d377ae846ee33c9d15daa19f9f9359e65da
Vasil Hristov <vasildimitrovhristov@gmail.com>
2019-08-09 10:26:57 +0300

    Enable bugsnag only on production

--- src/screens/utils/bugsnag.js
+++ src/screens/utils/bugsnag.js
@@ -2,7 +2,5 @@ import { Client, Configuration } from 'bugsnag-react-native'
 
 const configuration = new Configuration()
 configuration.apiKey = '3a04462591f10c8ef4e129d2d0c35a9d'
-configuration.notifyReleaseStages = ['beta', 'production']
-configuration.releaseStage = 'production'
-
+configuration.notifyReleaseStages = ['production']
 export const bugsnag = new Client(configuration)
