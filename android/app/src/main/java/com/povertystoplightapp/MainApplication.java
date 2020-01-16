7a73776a3850dd93d1c33a8224634b8d66362a64
Vasil Hristov <vasildimitrovhristov@gmail.com>
2020-01-16 14:54:26 +0200

    add react-native-languages package

--- android/app/src/main/java/com/povertystoplightapp/MainApplication.java
+++ android/app/src/main/java/com/povertystoplightapp/MainApplication.java
@@ -2,6 +2,7 @@ package com.povertystoplightapp;
 import com.povertystoplightapp.CustomDeletePackage;
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rnlanguages.RNLanguagesPackage;
 import com.bugsnag.BugsnagReactNative;
 import com.testfairy.react.TestFairyPackage;
 import com.christopherdro.htmltopdf.RNHTMLtoPDFPackage;
@@ -41,6 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
         
           new MainReactPackage(),
+            new RNLanguagesPackage(),
             BugsnagReactNative.getPackage(),
             new TestFairyPackage(),
           new CustomDeletePackage(),
