b2d002e8112dd56992e20d46cc0e2ccaf0de296b
JDelPuerto <jdelpuerto@codium.com.py>
2020-02-24 16:03:37 -0300

    Custom title added for overview

--- src/navigation/CustomHeaderSurvey.js
+++ src/navigation/CustomHeaderSurvey.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Platform } from 'react-native'
 import globalStyles from '../globalStyles'
+import { withNamespaces } from 'react-i18next'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import colors from '../theme.json'
 
 export class CustomHeaderSurveyComponent extends Component {
   render() {
-    const { navigation } = this.props
+    const { overview, navigation } = this.props
     const survey = navigation.getParam('survey')
-
+    const stoplightSkipped = this.props.navigation.getParam('stoplightSkipped')
     return (
       <View
         style={styles.headerQuestions}
@@ -20,24 +22,38 @@ export class CustomHeaderSurveyComponent extends Component {
         }}
         accessibilityLiveRegion="assertive"
       >
-        <Text style={styles.dimension}>
-          {survey.surveyStoplightQuestions[
-            navigation.state.params.step
-          ].dimension.toUpperCase()}
-        </Text>
-        <Text style={styles.title}>
-          {
-            survey.surveyStoplightQuestions[navigation.state.params.step]
-              .questionText
-          }
-        </Text>
+        {!overview && (
+          <Text style={styles.dimension}>
+            {survey.surveyStoplightQuestions[
+              navigation.state.params.step
+            ].dimension.toUpperCase()}
+          </Text>
+        )}
+        {!overview && (
+          <Text style={styles.title}>
+            {
+              survey.surveyStoplightQuestions[navigation.state.params.step]
+                .questionText
+            }
+          </Text>
+        )}
+        {overview && (
+          <Text style={styles.headerTitleStyle}>
+            {!stoplightSkipped
+              ? this.props.t('views.yourLifeMap')
+              : this.props.t('draftStatus.draft')}
+          </Text>
+        )}
       </View>
     )
   }
 }
 
 CustomHeaderSurveyComponent.propTypes = {
-  navigation: PropTypes.object.isRequired
+  navigation: PropTypes.object.isRequired,
+  t: PropTypes.func,
+  lng: PropTypes.string,
+  overview: PropTypes.bool
 }
 
 const styles = StyleSheet.create({
@@ -57,7 +73,28 @@ const styles = StyleSheet.create({
     ...globalStyles.h3,
     ...globalStyles.centerText,
     color: colors.dark
+  },
+  headerTitleStyle: {
+    ...Platform.select({
+      ios: {
+        fontFamily: 'Poppins'
+      },
+      android: {
+        fontFamily: 'Poppins SemiBold'
+      }
+    }),
+    fontSize: 18,
+    fontWeight: '200',
+    lineHeight: 26,
+    marginLeft: 20,
+    color: colors.black
   }
 })
 
-export default CustomHeaderSurveyComponent
+const mapStateToProps = ({ lng }) => ({
+  lng
+})
+
+export default withNamespaces()(
+  connect(mapStateToProps)(CustomHeaderSurveyComponent)
+)
