6a2266edb0f2033c47eb1af0d8b6c0ef9f279245
Guillecaba <guillermocaba95@gmail.com>
2020-09-25 16:35:07 -0400

    fix:remove bold in lifempa list

--- src/components/LifemapListItem.js
+++ src/components/LifemapListItem.js
@@ -10,7 +10,7 @@ class LifemapListItem extends Component {
     return (
       <ListItem style={{...styles.listItem}} onPress={this.props.handleClick}>
         <View style={styles.listItemContainer}>
-          <Text style={{...globalStyles.h3Bold, ...styles.h3Bold}}>
+          <Text style={{...globalStyles.h3, ...styles.h3}}>
             {this.props.name}
           </Text>
         </View>
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     flex: 1,
   },
-  h3Bold: {
+  h3: {
     paddingRight: 20,
     paddingTop: 36,
     alignSelf: 'center',
